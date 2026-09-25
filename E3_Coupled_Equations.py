8984de0a3d85694ec410a61c1915f74ad485e495
Aaron <aaron.dignam3@mail.dcu.ie>
2026-09-25 12:24:54 +0100

    Add Updated Scripts

--- E3_Coupled_Equations.py
+++ E3_Coupled_Equations.py
@@ -9,23 +9,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Variables
-h = 0.05 # Step
-tmax = 37.7 # Maximum time
-
-
-# Initial Conditions
-t = 0 # Start time
-x = 0.7 # Initial x value
-y = 0.7 # Initial y value (since dx/dt = y)
-
-
-# Arrays
-xval = np.array([x]) # Array of x values
-yval = np.array([y]) # Array of y values
-sinval = np.array([t])
-xyval = np.array([t])
-time = np.array([t]) # Time array
-
+h = [0.05, 0.005, 0.0005] # List of steps to run through
+tmax = 10 * np.pi # enough time for 5 oscillations
 
 # Defining Functions
 def dx_func(x,y,t):
@@ -36,50 +21,82 @@ def dy_func(x, y, t):
     dy = -x
     return dy
 
+# Creates one figure for each run
+fig, ax = plt.subplots() # Define graph for euler and actual values
+fig2, ax2 = plt.subplots() # Define graph for calculated difference between euler and actual
 
-# Loop which calculates new values
-while True:
-    # Calculate next time
-    t = t + h
-    # Actual Sine value
-    s = np.sin(t)
-    # Calculating x-value
-    x = x + dx_func(x,y,t) * h
-    print(x)
-    # Calculating y-value
-    y = y + dy_func(x,y,t) * h
-    print(y)
-    # Append values to arrays
-    xval = np.append(xval, [x]) # Append to xval array
-    yval = np.append(yval, [y]) # Append to yval array
-    time = np.append(time, [t]) # Append to time array
-    sinval = np.append(sinval, s) # Append to true value
-    sums = x + y # Adds x and y array together
-    xyval = np.append(xyval, sums) # Appends sum of x and y to array
-    #sleep(2)
-    # Loop ending condition
-    if abs(t - tmax) < h/2:
-        print(f"xval: {xval} // Size: {xval.size}")
-        print()
-        print(f"time: {time} // Size: {time.size}")
-        break
-    
+for i in h:
+    # h-value for run
+    h_new = i
+    # Initial Conditions
+    # Initial Conditions
+    t = 0 # Start time
+    x = 0 # Initial x value
+    y = 1 # Initial y value (since dx/dt = y)
+    diff = 0 # Initial difference value between actual and calculated
+    # Arrays
+    xval = np.array([x]) # Array of x values
+    yval = np.array([y]) # Array of y values
+    sinval = np.array([t])
+    time = np.array([t]) # Time array
+    diffval = np.array([diff])
+    # Loop which calculates new values
+    while True:
+        # Calculate next time
+        t = t + h_new
+        x_old, y_old = x, y
+        # Actual Sine value
+        s = np.sin(t)
+        # Calculating x-value
+        x = x_old + dx_func(x_old,y_old,t) * h_new
+        # Calculating y-value
+        y = y_old + dy_func(x_old,y_old,t) * h_new
+        # Calculating the difference
+        diff = x - s
+        # Append values to arrays
+        xval = np.append(xval, [x]) # Append to xval array
+        yval = np.append(yval, [y]) # Append to yval array
+        time = np.append(time, [t]) # Append to time array
+        sinval = np.append(sinval, [s]) # Append to true value
+        diffval = np.append(diffval, [diff]) # Append to difference in values array
+        
+        #sleep(2)
+        # Loop ending condition
+        if abs(t - tmax) < h_new/2:
+            #print(f"xval: {xval} // Size: {xval.size}")
+            #print()
+            #print(f"time: {time} // Size: {time.size}")
+            break
+        #print(f"Difference array = {diffval} // Size = {diffval.size}")
+    ax.plot(time, xval, label=f"Euler values for h = {h_new}") # Plotting euler on the graoh
+    ax2.plot(time, diffval, label=f"Difference values for h = {h_new}") # plotting difference values
+
+
 
 # print(f"xvalues = {xval} // Size = {xval.size}")
 # print(f"yvalues = {yval} // Size = {yval.size}")
 # print(f"Time-value array: {time} // Size: {time.size}")
 
-# Creates one figure for each run
-fig, ax = plt.subplots()
+
 # Plotting the results of the function
-ax.plot(time, xval, label='x-values')
-ax.plot(time, yval, label='y-values')
-ax.plot(time, sinval, label='Sine Values')
+
+#ax.plot(time, yval, label='y-values')
+ax.plot(time, sinval, label='Actual Values')
 #ax.plot(time, xyval, label='xy-values')
 
-ax.set(xlabel = 'Time (s)', ylabel = 'X & Y values')
+ax.set(xlabel = 'Time (s)', ylabel = 'x')
+ax2.set(xlabel='Time (s)', ylabel='Difference Value')
 ax.grid()
 ax.legend()
+ax2.grid()
+ax2.legend()
 plt.show()
 
+# Plotting just h = 0.0005 to see the difference values
 
+fig3, ax3 = plt.subplots()
+ax3.plot(time, diffval, label=f"Focused values for exclusively h = {h_new}")
+ax3.plot(xlabel='Time (s)', ylabel='Difference')
+ax3.legend()
+ax3.grid()
+plt.show()
